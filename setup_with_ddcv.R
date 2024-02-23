c6f0234711432d8a1178eb818ce894eff3321fe9
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-02-23 10:56:38 +0100

    updates

--- setup_with_ddcv.R
+++ setup_with_ddcv.R
@@ -1,11 +1,11 @@
 library(datadrivencv)
 library(here)
 use_datadriven_cv(
-  full_name = "Nick Strayer",
-  data_location = "https://docs.google.com/spreadsheets/d/14MQICF2F8-vf8CKPF1m4lyGKO6_thG-4aSwat1e2TWc",
-  pdf_location = "https://github.com/nstrayer/cv/raw/master/strayer_cv.pdf",
-  html_location = "nickstrayer.me/cv/",
-  source_location = "https://github.com/nstrayer/cv",
+  full_name = "Aleksander Bang-Larsen",
+  data_location = "https://docs.google.com/spreadsheets/d/1c23M18bAcy3k8gQCXM2dDgo_5yP50fYWrKidRnjQa2Y/edit?usp=sharing",
+  pdf_location = "https://github.com/aleksanderbl29/cv-from-r/raw/main/bang-larsen_cv.pdf",
+  html_location = "cv.aleksanderbl.dk",
+  source_location = "https://github.com/aleksanderbl29/cv-from-r",
   output_dir = here(),
   open_files = FALSE
 )
