c87d461076e979741a5a006bf9112fd91954b0e3
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-02-23 10:56:43 +0100

    test packages

---
+++ test.r
@@ -0,0 +1,9 @@
+
+install.packages("icon")
+devtools::install_github('Ash706/datadrivencv')
+
+datadrivencv::use_datadriven_cv(full_name = "Aleksander Bang-Larsen")
+
+
+# install.packages("remotes")
+remotes::install_github("mitchelloharawild/icons")
