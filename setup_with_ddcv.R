cda4b4318fefab81d661a9e63afca9e629bdf3f7
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-02-23 11:00:55 +0100

    change pdf link

--- setup_with_ddcv.R
+++ setup_with_ddcv.R
@@ -3,7 +3,7 @@ library(here)
 use_datadriven_cv(
   full_name = "Aleksander Bang-Larsen",
   data_location = "https://docs.google.com/spreadsheets/d/1c23M18bAcy3k8gQCXM2dDgo_5yP50fYWrKidRnjQa2Y/edit?usp=sharing",
-  pdf_location = "https://github.com/aleksanderbl29/cv-from-r/raw/main/bang-larsen_cv.pdf",
+  pdf_location = "https://github.com/aleksanderbl29/cv-from-R/blob/master/cv.pdf",
   html_location = "cv.aleksanderbl.dk",
   source_location = "https://github.com/aleksanderbl29/cv-from-r",
   output_dir = here(),
